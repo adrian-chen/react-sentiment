b31d69564e57c5d0e6f75fa727c1c632a9d92b91
Adrian Chen <adrianchen@Adrians-MacBook-Pro.local>
2015-03-15 14:13:49 -0600

    finished

---
+++ js/doctor/view.js
@@ -0,0 +1,13 @@
+var DoctorView = React.createClass({
+
+    getInitialState: function() {
+        return {doctor: null}
+    },
+
+    render: function() {
+        
+            return (
+                <h4>Enter a URL (or two) and hit <i>Go!</i> to do some sentiment analysis.</h4>
+            )
+    }
+})
